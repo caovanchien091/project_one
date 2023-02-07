0db18a0b0eb0fe22b1b41e9a7be2b6277a3caa90
caovanchien000 <caovanchien000@gmail.com>
2023-02-07 17:35:20 +0700

    No Message

--- module/share/common/lib/src/router/app_page_route.dart
+++ module/share/common/lib/src/router/app_page_route.dart
@@ -1,37 +1,44 @@
-import 'package:core/core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class AppPageRoute<T> extends MaterialPageRoute<T> {
+class AppPageRoute<T> extends PageRoute<T> {
   final List<Bloc>? blocs;
+  final WidgetBuilder builder;
 
   AppPageRoute({
     this.blocs,
+    required this.builder,
     required RouteSettings settings,
-    required WidgetBuilder builder,
-  }) : super(builder: builder, settings: settings);
+  }) : super(settings: settings);
 
   @override
-  Widget buildContent(BuildContext context) {
-    if (blocs.isEmpty) {
-      return Scaffold(
-        body: super.buildContent(
-          context,
-        ),
-      );
-    }
+  Color? get barrierColor => null;
 
-    return Scaffold(
-      body: MultiBlocProvider(
-        providers: blocs.mapTo(
-          (item) => BlocProvider(
-            create: (context) => item,
-          ),
-        ),
-        child: super.buildContent(
-          context,
-        ),
+  @override
+  String? get barrierLabel => null;
+
+  @override
+  Widget buildPage(context, animation, secondaryAnimation) {
+    return builder(context);
+  }
+
+  @override
+  Widget buildTransitions(BuildContext context, animation, secondaryAnimation, child) {
+    return Container(
+      color: Colors.blue,
+      child: SlideTransition(
+        position: Tween<Offset>(
+          begin: const Offset(-1, 0),
+          end: Offset.zero,
+        ).animate(animation),
+        child: child,
       ),
     );
   }
+
+  @override
+  bool get maintainState => true;
+
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 500);
 }
