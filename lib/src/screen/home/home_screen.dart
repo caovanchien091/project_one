a57bd0ba1eaa61935eee958ebcb9c4862f89a48e
caovanchien000 <caovanchien000@gmail.com>
2023-02-09 17:45:49 +0700

    Update at the end of the day

--- lib/src/screen/home/home_screen.dart
+++ lib/src/screen/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:project_one/src/register/app_route.dart';
 import 'package:theme/theme.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -16,43 +17,17 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: CircleLoading(),
-      ),
-    );
-  }
-}
-
-class CircleLoading extends StatefulWidget {
-  const CircleLoading({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<CircleLoading> createState() => _CircleLoadingState();
-}
-
-class _CircleLoadingState extends State<CircleLoading> with TickerProviderStateMixin {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            AppIcon(
-              size: 100,
-              iconData: Icons.add,
-              color: Colors.blue,
-              background: Colors.yellow,
-            ),
-          ],
+      body: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            child: const Text("Chien"),
+            onPressed: () {
+              Navigator.of(context).pushNamed(AppRoute.testScreen);
+            },
+          ),
         ),
       ),
     );
   }
 }
-
-
